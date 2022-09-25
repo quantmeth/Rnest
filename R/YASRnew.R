aaa1f962cf52deb7591c3c051f2053f61734b6d2
P.-.O Caron <41082179+quantmeth@users.noreply.github.com>
2022-09-25 12:25:10 -0400

    revision for simulations

--- R/YASRnew.R
+++ R/YASRnew.R
@@ -10,14 +10,12 @@
 #' @export
 #'
 #' @examples
-#' \dontrun{
-#' YASRnew(gen_data(x_4factors_corr))
-#' }
+#' YASRnew(ex_4factors_corr, n.obs = 42)
 YASRnew <- function(data, 
-                    n.obs = NULL,
-                    alpha = .05, 
-                    max.fact = max(which(dof(nv)>(nv/2)))-1,
-                    ...){
+                 n.obs = NULL,
+                 alpha = .05, 
+                 max.fact = max(which(dof(nv)>(nv/2)))-1,
+                 ...){
   # CHECK max.fact's dof < 0
   
   if(isSymmetric.matrix(data)){
@@ -42,6 +40,7 @@ YASRnew <- function(data,
   if((length(Re(eig)) != nv) && (sum(eig) != nv)){
     stop("Correlation matrix is not positive semi definite")
   }
+  
   RR <- cor(data)
   Rt <- RR[lower.tri(RR)]
   res <- as.data.frame(matrix(0, ncol = 1,
@@ -52,19 +51,25 @@ YASRnew <- function(data,
     if(i == 1){
       RRR <- diag(nv)
     } else{
-      fa <- factanal(covmat = RR, n.obs = n.obs, factors = i-1, ...) #,...
+      fa <- factanal(covmat = RR, n.obs = n.obs, factors = i-1,...) #,...
       ld <- cbind(fa$loadings, diag(sqrt(fa$uniquenesses)))
       RRR <- ld %*% t(ld)
     }
     R.test <- RRR[lower.tri(RRR)]
     z <- (atanh(Rt)-atanh(R.test))*sqrt(n.obs-3)
     res$stat[i] <- sum(z^2)
+    res$dof[i] <- dof(nv,i-1)
+    res$crit[i] <- qchisq(1-alpha,res$dof[i])
+    res$pv[i] <- round(pchisq(res$stat[i], res$dof[i], lower = FALSE), 5)
+    res$sig[i] <- (res$stat[i] >= res$crit[i])
+    if(!res$sig[i]) break
   }
-  res$dof <- dof(nv,0:max.fact)
-  res$crit <- qchisq(.95,res$dof)
-  res$pv <- round(pchisq(res$stat, res$dof, lower.tail = FALSE), 5)
-  res$sig <- res$stat >= res$crit 
-  nfactors <-   min(which(!res$sig))-1
+  res <- res[1:i,]
+  # res$dof <- dof(nv,0:max.fact)
+  # res$crit <- qchisq(1-alpgha,res$dof)
+  # res$pv <- round(pchisq(res$stat, res$dof, lower = FALSE), 5)
+  # res$sig <- res$stat >= res$crit 
+  nfactors <- min(which(!res$sig))-1
   return(list(nfactors = nfactors, results = res))
 }
 
@@ -74,18 +79,6 @@ dof <- function(nv, i = 0:nv){
 
 gen_data <- function(R, n = 20*ncol(R)+3){
   MASS::mvrnorm(n = n, Sigma = R, mu = rep(0, ncol(R)))
-}
-
-tri.l <- function(x, lower = TRUE, diag = FALSE){
-  if(lower){
-    x[lower.tri(x, diag = diag)]
-  }else{
-    x[upper.tri(x, diag = diag)]
-  }
-}
-
-tri.u <- function(x, lower = FALSE, diag = FALSE){
-  tri.l(x, lower = lower, diag = diag)
 }
 
 #multivariate case
