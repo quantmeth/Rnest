f2fe1a852e6c236193054f95d22e38692f22d0b9
P.-.O Caron <41082179+quantmeth@users.noreply.github.com>
2022-09-26 09:38:40 -0400

    1.58

--- R/YASR1.58.R
+++ R/YASR1.58.R
@@ -1,8 +1,9 @@
-#' Yet Another Stopping Rule (new) chi2(atanh)
+#' Yet Another Stopping Rule (1.58)
 #'
 #' @param data A data frame, a numeric matrix, covariance matrix or correlation matrix from which to determine the number of factors.
 #' @param n.obs The number of cases (subjects, participants, or units) if a covariance matrix is supplied in \code{data}.
 #' @param alpha Type I error rate.
+#' @param nreps Number of resamples.
 #' @param max.fact  An optional maximum number of factor to extract.
 #' @param ... Arguments for methods.
 #'
@@ -11,40 +12,46 @@
 #'
 #' @examples 
 #' \dontrun{
-#' YASRnew(gen_data(x_4factors_corr))
+#' YASR1.58(gen_data(x_4factors_corr))
 #' }
 YASRnew <- function(data, 
-                 n.obs = NULL,
-                 alpha = .05, 
-                 max.fact = max(which(dof(nv)>(nv/2)))-1,
-                 ...){
+                    n.obs = NULL,
+                    alpha = .05, 
+                    nreps = 1000,
+                    max.fact = max(which(dof(nv)>(nv/2)))-1,
+                    ...){
   # CHECK max.fact's dof < 0
   
   if(isSymmetric.matrix(data)){
     
     if(all(diag(data) == 1)){
-      R <- data
+      RR <- data
     }else{
-      R <- cov2cor(data)
+      RR <- cov2cor(data)
     }
     
     if(is.null(n.obs)){
       stop("argument \"n.obs\" is missing with covariance matrix")
     }
   } else {
-    R <- cor(data)
+    RR <- cor(data)
     n.obs <- nrow(data)
   }
   
   nv <- ncol(data)
-  eig <- t(as.matrix(eigen(R, symmetric = TRUE, only.values = TRUE)$values))
+  eig <- t(as.matrix(eigen(RR, symmetric = TRUE, only.values = TRUE)$values))
   
   if((length(Re(eig)) != nv) && (sum(eig) != nv)){
     stop("Correlation matrix is not positive semi definite")
   }
-  
-  RR <- cor(data)
+  ndl <- dof(nv,0)
   Rt <- RR[lower.tri(RR)]
+  
+  rr <- replicate(nreps, gen_R(RR, n.obs))
+  za <- (atanh(rr)-atanh(Rt)) * sqrt(n.obs-3)
+  zs <- colSums(za^2)/ndl
+  crit <- quantile(zs, 1-alpha) #1.582; 1.57
+ 
   res <- as.data.frame(matrix(0, ncol = 1,
                               nrow = max.fact + 1,
                               dimnames = list(0:max.fact,
@@ -59,19 +66,13 @@ YASRnew <- function(data,
     }
     R.test <- RRR[lower.tri(RRR)]
     z <- (atanh(Rt)-atanh(R.test))*sqrt(n.obs-3)
-    res$stat[i] <- sum(z^2)
-    res$dof[i] <- dof(nv,i-1)
-    res$crit[i] <- qchisq(1-alpha,res$dof[i])
-    res$pv[i] <- round(pchisq(res$stat[i], res$dof[i], lower.tail = FALSE), 5)
-    res$sig[i] <- (res$stat[i] >= res$crit[i])
+    res$stat[i] <- sum(z^2)/ndl
+    res$sig[i] <- res$stat[i] > crit
     if(!res$sig[i]) break
   }
   res <- res[1:i,]
-  # res$dof <- dof(nv,0:max.fact)
-  # res$crit <- qchisq(1-alpgha,res$dof)
-  # res$pv <- round(pchisq(res$stat, res$dof, lower.tail = FALSE), 5)
-  # res$sig <- res$stat >= res$crit 
   nfactors <- min(which(!res$sig))-1
+  #res; crit; nfactors
   return(list(nfactors = nfactors, results = res))
 }
 
